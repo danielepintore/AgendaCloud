0e86848af1336dae74c0c0c4648b79babb71939d
daniele <pintdany@gmail.com>
2022-01-28 13:16:56 +0100

    Update of 28/01/2022

--- node/build.sh
+++
@@ -1,3 +0,0 @@
-cp node_modules/bootstrap/dist/css/bootstrap.min.css public/css
-cp node_modules/bootstrap/dist/js/bootstrap.bundle.min.js public/js
-cp node_modules/jquery/dist/jquery.min.js public/js
