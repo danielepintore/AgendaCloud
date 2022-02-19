3ab2e305c2453fc1ef64a9a70cb927a4fb7ead0b
daniele <pintdany@gmail.com>
2022-02-19 02:22:34 +0100

    Added a config.php file with all variables fetched from .env file, the file is loaded using vlucas/phpdotenv lib

---
+++ app/config/config.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ * From composer we load the dotEnv module and we fetch the variables that are located in the project .env file
+ */
+
+require '../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable("../");
+$dotenv->load();
+$config = array(
+    "db" => array(
+        "dbname" => $_ENV['DB_NAME'],
+        "username" => $_ENV['DB_USER'],
+        "password" => $_ENV['DB_PWD'],
+        "host" => $_ENV['DB_HOST'],
+    ),
+    /*
+     * The environment array can be like this:
+     * name variable can be: production, debug
+     * debug value can be true or false
+     */
+    "environment" => array(
+        "name" => "debug", // TODO: change to production and disable debug
+        "debug" => true,
+    ),
+    "urls" => array(
+        "baseUrl" => "http://" . $_ENV['DOMAIN_NAME'] // TODO change to https in production
+    ),
+);
+
+/*
+    Creating constants for heavily used paths makes things a lot easier.
+    ex. require_once(LIBRARY_PATH . "Paginator.php")
+*/
+defined("BASE_PATH")
+or define("BASE_PATH", realpath(dirname(__FILE__, 2)));
+
+defined("SRC_PATH")
+or define("SRC_PATH", BASE_PATH . '/src');
+
+defined("TEMPLATES_PATH")
+or define("TEMPLATES_PATH", BASE_PATH . '/templates');
+
+defined("PUBLIC_PATH")
+or define("PUBLIC_PATH", BASE_PATH . '/public');
+
+defined("COMPOSER_AUTOLOAD")
+or define("COMPOSER_AUTOLOAD", BASE_PATH . '/vendor/autoload.php');
+
+/*
+    Error reporting
+*/
+if ($config["environment"]["debug"]) {
+    // set error reporting to all
+    error_reporting(E_ALL);
+} else {
+    // otherwise, disable error reporting
+    error_reporting(0);
+}
+?>
