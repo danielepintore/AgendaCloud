9137090219d83d1f013e087ee332f23df4a981c5
daniele <pintdany@gmail.com>
2022-02-21 22:12:22 +0100

    New query that gets only the needed data

--- app/src/Service/services.php
+++ app/src/Service/services.php
@@ -5,14 +5,15 @@ class Services {
         require_once(realpath(dirname(__FILE__, 3)) . '/vendor/autoload.php');
         try {
             $db = Database::getDB();
-            $sql = "SELECT * FROM Servizio WHERE(IsActive = TRUE)";
+            $sql = "SELECT id, Nome, Durata, OraInizio, OraFine, Costo FROM Servizio WHERE(IsActive = TRUE)";
             $stmt = $db->prepare($sql);
             if ($stmt->execute()) {
                 //Success
                 $result = $stmt->get_result();
                 $response = array();
                 foreach ($result as $r) {
-                    $response[] = $r;
+                    $response[] = array('id' => $r['id'], 'Nome' => $r['Nome'], 'Durata' => $r['Durata'],
+                        'OraInizio' => $r['OraInizio'], 'OraFine' => $r['OraFine'], 'Costo' => $r['Costo']);
                 }
                 return (array("error" => false, "response" => $response));
             } else {
