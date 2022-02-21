c4c24e3a64306e50716ec952c9af3f9ab2f4af02
daniele <pintdany@gmail.com>
2022-02-21 22:07:18 +0100

    Delete the unused method to get all services data

--- app/public/api/get_services.php
+++ app/public/api/get_services.php
@@ -3,18 +3,16 @@ require_once(realpath(dirname(__FILE__, 3)) . '/src/Api/loader.php');
 if (isset($_GET['serviceId']) && is_numeric($_GET['serviceId'])) {
     $serviceObject = new Service($_GET['serviceId']);
     $services = $serviceObject->getServiceInfo();
-} else {
-    $services = Services::getAllServices();
-}
-// se non ci sono stati errori fornisci la risposta
-if (!$services["error"]) {
-    if (count($services["response"]) == 0) {
-        print(json_encode(array()));
+    // se non ci sono stati errori fornisci la risposta
+    if (!$services["error"]) {
+        if (count($services["response"]) == 0) {
+            print(json_encode(array()));
+        } else {
+            print(json_encode($services["response"]));
+        }
     } else {
-        print(json_encode($services["response"]));
+        // TODO: send log
+        // there is an error
+        print(json_encode(array("error" => true)));
     }
-} else {
-    // TODO: send log
-    // there is an error
-    print(json_encode(array("error" => true)));
 }
