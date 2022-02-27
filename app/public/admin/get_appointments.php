a215f0ad110f7b3ced30050642d873faea7235ed
daniele <pintdany@gmail.com>
2022-02-27 01:43:20 +0100

    admin appointment api endpoint

---
+++ app/public/admin/get_appointments.php
@@ -0,0 +1,32 @@
+<?php
+use Admin\User;
+require_once(realpath(dirname(__FILE__, 3)) . '/src/Api/loader.php');
+session_start();
+if (session_status() == PHP_SESSION_ACTIVE && $_SESSION['logged']) {
+    // user is logged
+    // create user object
+    $user = new User($_SESSION['logged'], $_SESSION['username'], $_SESSION['password'], $_SESSION['isAdmin']);
+} else {
+    // user isn't logged
+    // redirect to login page
+    header("HTTP/1.1 303 See Other");
+    header("Location: /admin/index.php");
+}
+if (isset($_GET['date'])) {
+    // create a service object
+    try {
+        $appointments = \Admin\Appointment::getAppointments($_SESSION['isAdmin'], $_GET['date']);
+        // se non ci sono stati errori fornisci la risposta
+        if (count($appointments) == 0) {
+            print(json_encode(array()));
+        } else {
+            print(json_encode($appointments));
+        }
+    } catch (DatabaseException|Exception $e) {
+        print(json_encode(array("error" => true)));
+        die(0);
+    }
+} else {
+    print(json_encode(array("error" => true)));
+    die(0);
+}
