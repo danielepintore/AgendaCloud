aaec68cf38917f57ce95197d0f9cbc41c6c50c79
Daniele Pintore <daniele.pintore1@gmail.com>
2022-06-15 23:12:14 +0200

    Beautify jsonUtility.php

--- app/src/JSON/jsonUtility.php
+++ app/src/JSON/jsonUtility.php
@@ -1,9 +1,18 @@
 <?php
 
+/**
+ * This class contains the code to work with JSON file in php
+ */
 class JSONUtility {
+
+    /**
+     * @param string $json
+     * @return bool
+     * This function check if the JSON passed is a valid JSON, if it is returns true, otherwise false
+     */
     public static function validateJSON(string $json): bool {
         try {
-            $test = json_decode($json, null, flags: JSON_THROW_ON_ERROR);
+            json_decode($json, null, flags: JSON_THROW_ON_ERROR);
             return true;
         } catch (Exception $e) {
             return false;
