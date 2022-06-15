5af86093260d57383e883f7a1c99a7bf7cba67da
Daniele Pintore <daniele.pintore1@gmail.com>
2022-06-15 22:31:57 +0200

    beautify payment.php

--- app/src/Payment/payment.php
+++ app/src/Payment/payment.php
@@ -1,33 +1,43 @@
 <?php
 
+/**
+ * This class contains all function used to manage all payment methods
+ */
+
 class Payment {
     /**
-     * @return array
      * @throws DatabaseException
+     * @return array{
+     *     id: int,
+     *     name: string
+     *     }
+     * Returns a list of ACTIVE payments method
      */
-    public static function getActivePaymentMethods(Database $db) {
+    public static function getActivePaymentMethods(Database $db): array {
         require_once(realpath(dirname(__FILE__, 3)) . '/vendor/autoload.php');
         $sql = "SELECT * FROM MetodoPagamento WHERE IsActive = TRUE";
         $status = $db->query($sql);
-        if ($status) {
-            //Success
-            $result = $db->getResult();
-            $response = [];
-            foreach ($result as $r) {
-                $response[] = array("id" => $r["id"], "name" => $r["Nome"]);
-            }
-            return $response;
-        } else {
+        if (!$status) {
             throw DatabaseException::queryExecutionFailed();
         }
+        $result = $db->getResult();
+        $response = [];
+        foreach ($result as $r) {
+            $response[] = array("id" => $r["id"], "name" => $r["Nome"]);
+        }
+        return $response;
     }
 
     /**
-     * @return array
-     * @throws \DatabaseException
-     * Return all payments method availables
+     * @throws DatabaseException
+     * @return array{
+     *     id: int,
+     *     name: string,
+     *     isActive: bool
+     *     }
+     * Returns the complete list of payments method available
      */
-    public static function getPaymentMethods(\Database $db) {
+    public static function getPaymentMethods(Database $db): array {
         require_once(realpath(dirname(__FILE__, 3)) . '/vendor/autoload.php');
         $sql = "SELECT * FROM MetodoPagamento";
         $status = $db->query($sql);
@@ -48,20 +58,21 @@ class Payment {
     }
 
     /**
-     * @throws \DatabaseException
-     * Updates the status of a payment method
+     * @throws DatabaseException
+     * @return bool
+     * Updates the status of a payment method identified by its id
+     * Returns true on success and false on failure
      */
-    public static function updatePaymentMethodStatus(\Database $db, $paymentMethodId, $status) {
+    public static function updatePaymentMethodStatus(Database $db, $paymentMethodId, $status): bool {
         require_once(realpath(dirname(__FILE__, 3)) . '/vendor/autoload.php');
         $config = Config::getConfig();
         //cash cannot be disabled
         if ($paymentMethodId == CASH){
             return false;
         }
-        // if credit card isn't configured don't enable it
+        // if credit card isn't configured we can't enable it
         if ($paymentMethodId == CREDIT_CARD &&
             (empty($config->stripe->secret_api_key) || empty($config->stripe->endpoint_secret))) {
-            print("nope");
             return false;
         }
         $sql = "UPDATE MetodoPagamento SET isActive = ? WHERE MetodoPagamento.id = ?";
@@ -75,7 +86,10 @@ class Payment {
     }
 
     /**
-     * Check if the payment method provided is valid and active
+     * @param Database $db
+     * @param $methodId
+     * @return bool
+     * Given a specific identifier checks if the method is valid
      */
     public static function isAValidMethod(Database $db, $methodId) {
         require_once(realpath(dirname(__FILE__, 3)) . '/vendor/autoload.php');
@@ -85,6 +99,7 @@ class Payment {
             $status = $db->query($sql, "i", $methodId);
             $result = $db->getResult();
             if ($status && $db->getAffectedRows() == 1) {
+                // checks if the credit card payments are configured properly
                 if ($methodId == CREDIT_CARD &&
                     (empty($config->stripe->secret_api_key) || empty($config->stripe->endpoint_secret))) {
                     return false;
@@ -93,16 +108,29 @@ class Payment {
                     return true;
                 }
             }
+            return false;
         } catch (Exception $e){
+            if (DEBUG){
+                Debug::printException($e);
+            }
             return false;
         }
-        return false;
     }
 
-    public static function isCashSelected($methodId) {
-        // credit card = 1
-        // cash = 2
-        // if we add multiple payment methods we need to update this method but because i have only cash and credit card is fine
+    /**
+     * @param $methodId
+     * @return bool
+     * Given a method id check if is the one corresponding to cash
+     */
+    public static function isCashSelected($methodId): bool {
+        /* *************************************************
+        credit card = 1
+        cash = 2
+        More info can be found on Constants class
+
+        if we add multiple payment methods we need to update this method but
+        because we have only cash and credit card is fine
+        ************************************************* */
         if ($methodId == CASH) {
             return true;
         } else {
