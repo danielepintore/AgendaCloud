3ba2616f4ee6ca034813b6e70438b091780985b3
Daniele Pintore <daniele.pintore1@gmail.com>
2022-06-15 23:45:46 +0200

    Beutify database.php

--- app/src/Database/database.php
+++ app/src/Database/database.php
@@ -1,13 +1,17 @@
 <?php
 
+/**
+ * Handles the connection to the database
+ */
 class Database {
 
-    public $db;
-    private $isResultAvailabe;
-    private $lastQueryStmt;
+    public mysqli $db;
+    private bool $isResultAvailabe;
+    private mysqli_stmt|false $lastQueryStmt;
 
     /**
      * @throws DatabaseException
+     * Creates the database object
      */
     public function __construct() {
         require_once(realpath(dirname(__FILE__, 3)) . '/vendor/autoload.php');
@@ -18,11 +22,15 @@ class Database {
                 throw DatabaseException::connectionFailed();
             }
         } catch (Exception $e) {
-            $textBody = $e->getMessage() . ": " . $e->getFile() . ":" . $e->getLine() . "\n" . $e->getTraceAsString() . "\n" . $e->getCode();
+            $textBody = Debug::getDebugMessage($e);
             try {
+                //TODO find another method to warn me
                 $phpMailer = new MailClient();
                 $phpMailer->sendEmail("Errore nella connessione con il DB", $textBody, $textBody, $config->mail->supervisor);
             } catch (Exception $e) {
+                if (DEBUG) {
+                    Debug::printException($e);
+                }
             }
             throw DatabaseException::connectionFailed();
         }
@@ -30,54 +38,45 @@ class Database {
 
     /**
      * @param string $sql
-     * @param string $format
+     * @param string|null $format
      * @param ...$args
      * @return bool
      * @throws DatabaseException
-     * This function is a wrapper for executing queries all checks of the input are performed by the
-     * internal functions
+     * This function is a wrapper for executing queries. All checks of the input are performed by the
+     * internal functions of mysqli
      */
-    public function query(string $sql, string $format = null, ...$args){
+    public function query(string $sql, string $format = null, ...$args): bool {
         $stmt = $this->db->prepare($sql);
         // check if the query have some errors
-        if ($stmt) {
-            // check if we have parameters
-            if (!empty($format)){
-                // bind the parameters of the query and then execute the query
-                if ($stmt->bind_param($format, ...$args)) {
-                    if ($stmt->execute()) {
-                        //Success
-                        $this->isResultAvailabe = true;
-                        $this->lastQueryStmt = $stmt;
-                        return true;
-                    } else {
-                        $this->isResultAvailabe = false;
-                        throw DatabaseException::queryExecutionFailed();
-                    }
-                } else {
-                    $this->isResultAvailabe = false;
-                    throw DatabaseException::bindingParamsFailed();
-                }
-            } else {
-                // execute the query
-                if ($stmt->execute()) {
-                    //Success
-                    $this->isResultAvailabe = true;
-                    $this->lastQueryStmt = $stmt;
-                    return true;
-                } else {
-                    $this->isResultAvailabe = false;
-                    throw DatabaseException::queryExecutionFailed();
-                }
-            }
-        } else {
+        if (!$stmt) {
             $this->isResultAvailabe = false;
             throw DatabaseException::queryPrepareFailed();
         }
+        // if we have parameters
+        if (!empty($format)) {
+            // bind the parameters of the query and then execute the query
+            if (!$stmt->bind_param($format, ...$args)) {
+                $this->isResultAvailabe = false;
+                throw DatabaseException::bindingParamsFailed();
+            }
+        }
+        if (!$stmt->execute()) {
+            $this->isResultAvailabe = false;
+            throw DatabaseException::queryExecutionFailed();
+        }
+        // Success
+        $this->isResultAvailabe = true;
+        $this->lastQueryStmt = $stmt;
+        return true;
     }
 
-    public function getResult() {
-        if ($this->isResultAvailabe){
+    /**
+     * @return array
+     * @throws DatabaseException
+     * Gets the result from the query
+     */
+    public function getResult(): array {
+        if ($this->isResultAvailabe) {
             $stmt_result = $this->lastQueryStmt->get_result();
             $this->isResultAvailabe = false;
             return $stmt_result->fetch_all(MYSQLI_ASSOC);
@@ -86,14 +85,25 @@ class Database {
         }
     }
 
-    public function getAffectedRows(){
-            return $this->lastQueryStmt->affected_rows;
+    /**
+     * @return int|string
+     * Gets the number of affected rows
+     */
+    public function getAffectedRows(): int|string {
+        return $this->lastQueryStmt->affected_rows;
     }
 
-    public function getInsertId(){
+    /**
+     * @return int|string
+     * Gets the last insert id
+     */
+    public function getInsertId(): int|string {
         return $this->lastQueryStmt->insert_id;
     }
 
+    /**
+     * Close the database connection
+     */
     public function __destruct() {
         $this->db->close();
     }
