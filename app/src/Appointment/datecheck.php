ec750eab70916b916553c1387b41661ab69c7354
Daniele Pintore <daniele.pintore1@gmail.com>
2022-08-07 00:40:04 +0200

    Beautify datecheck.php

--- app/src/Appointment/datecheck.php
+++ app/src/Appointment/datecheck.php
@@ -3,10 +3,12 @@
 class DateCheck {
     /**
      * @param $date
+     * @param bool $isUserAuthenticated
      * @return bool
      * @throws DateException
+     * Returns true if the date is valid
      */
-    public static function isValidDate($date, $isUserAuthenticated = false) {
+    public static function isValidDate($date, bool $isUserAuthenticated = false): bool {
         $config = Config::getConfig();
         try {
             $date = new DateTime($date);
@@ -22,21 +24,26 @@ class DateCheck {
         } catch (Exception $e) {
             throw DateException::invalidData();
         }
-
         try {
             $maxDateInterval = new DateInterval("P" . $config->calendar->max_future_day . "D");
         } catch (Exception $e) {
             throw DateException::wrongIntervalString();
         }
         if ($date <= $maxDate->add($maxDateInterval) && $date >= $now) {
-            // the date shouldn't be in the past and cant'b be bigger than maxdate
+            // the date shouldn't be in the past and can't be bigger than maxdate
             return true;
         } else {
             throw DateException::invalidData();
         }
     }
 
-    public static function isToday($date) {
+    /**
+     * @param string $date
+     * @return bool
+     * @throws DateException
+     * Returns true if the date passed is today
+     */
+    public static function isToday(string $date): bool {
         try {
             $date = new DateTime($date);
         } catch (Exception $e) {
