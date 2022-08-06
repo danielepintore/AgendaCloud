761576fee4e1f2d4fe21d66beed52ec543046321
Daniele Pintore <daniele.pintore1@gmail.com>
2022-08-06 03:34:56 +0200

    Refactor of the get slot method, still requires a checkup

--- app/src/Appointment/slot.php
+++ app/src/Appointment/slot.php
@@ -16,13 +16,13 @@ class Slot {
      * This function given the service identifier, the employee id and the date gives
      * all the slots available
      */
-    public static function getSlots(Database $db, $serviceId, $employeeId, $dateStr, $isUserAuthenticated = false) {
+    public static function getSlotsOld(Database $db, $serviceId, $employeeId, $dateStr, $isUserAuthenticated = false) {
         require_once(realpath(dirname(__FILE__, 3)) . '/vendor/autoload.php');
         DateCheck::isValidDate($dateStr, $isUserAuthenticated);
         $weekDay = (new DateTime($dateStr))->format('N');
         $workingTimes = workingTimes::getWorkingTimes($db, $weekDay, $dateStr, $serviceId, $employeeId);
         // get holiday start and end time
-        $holidayInfo = workingTimes::getHolidayTimes($db, $weekDay, $dateStr, $serviceId, $employeeId);
+        $holidayTimes = workingTimes::getHolidayTimes($db, $weekDay, $dateStr, $serviceId, $employeeId);
         // check if the employee is active
         if (\Admin\Employee::isActive($db, $employeeId)) {
             // get service data from the database
@@ -40,12 +40,12 @@ class Slot {
                         //Success
                         $result = $db->getResult();
                         // we need to initialize this variable because if we don't do this php will throw an exception
-                        $orari = []; // this variable will contain all already taken slots
+                        $bookedSlots = []; // this variable will contain all already taken slots
                         foreach ($result as $r) {
                             try {
                                 $startDate = new DateTime($r["OraInizio"]);
                                 $endDate = new DateTime($r["OraFine"]);
-                                $orari[] = array("start_time" => $startDate->format('H:i'), "end_time" => $endDate->format('H:i'));
+                                $bookedSlots[] = array("start_time" => $startDate->format('H:i'), "end_time" => $endDate->format('H:i'));
                             } catch (Exception $e) {
                                 throw DateException::wrongStartOrEndTime();
                             }
@@ -78,7 +78,7 @@ class Slot {
                                 continue;
                             }
                             $isFree = true;
-                            foreach ($orari as $o) {
+                            foreach ($bookedSlots as $o) {
                                 if ($interval->getStartTime() < $o["start_time"] && $interval->getEndTime() <= $o["start_time"] ||
                                     $interval->getStartTime() > $o["start_time"] && $interval->getStartTime() >= $o["end_time"]) {
                                     // lo slot potrebbe essere libero
@@ -102,9 +102,9 @@ class Slot {
                             $date->add($tempoIntervallo);
                         } while ($date < $endDate);
                         // if there is a holidays remove some slots
-                        if (sizeof($holidayInfo) > 0) {
+                        if (sizeof($holidayTimes) > 0) {
                             $slotsWithHolidays = [];
-                            foreach ($holidayInfo as $holiday) {
+                            foreach ($holidayTimes as $holiday) {
                                 foreach ($generated_slots as $slot) {
                                     // add only the slots that arent included in the holiday timeframe
                                     if ($slot["start_time"] <= $holiday['startTime'] && $slot['end_time'] <= $holiday['startTime']) {
@@ -127,5 +127,96 @@ class Slot {
             throw EmployeeException::employeeIsNotActive();
         }
         return [];
+    }
+
+    public static function getSlots(Database $db, $serviceId, $employeeId, $dateStr, $isUserAuthenticated = false) {
+        require_once(realpath(dirname(__FILE__, 3)) . '/vendor/autoload.php');
+        DateCheck::isValidDate($dateStr, $isUserAuthenticated);
+        $weekDay = (new DateTime($dateStr))->format('N');
+        $workingTimes = workingTimes::getWorkingTimes($db, $weekDay, $dateStr, $serviceId, $employeeId);
+        // check if the employee is active
+        if (!\Admin\Employee::isActive($db, $employeeId)) {
+            throw EmployeeException::employeeIsNotActive();
+        }
+        // get service data from the database
+        $service = new Service($db, $serviceId);
+        $bookedSlots = Appointment::getBookedAppointment($db, $employeeId, $dateStr);
+        // if the user is authenticated we can disable the BookableUntil time generatedSlot
+        if($isUserAuthenticated){
+            $service->setBookableUntil(0);
+        }
+
+        // generazione slots liberi
+        try {
+            $intervalTime = $service->getDuration() + $service->getWaitTime();
+            $serviceDuration = new DateInterval("PT" . $service->getDuration() . "M");
+            $totalInterval = new DateInterval("PT" . $intervalTime . "M");
+            $waitInterval = new DateInterval("PT" . $service->getWaitTime() . "M");
+            $bookableUntilInterval = new DateInterval("PT" . $service->getBookableUntil() . "M");
+        } catch (ServiceException $serviceException){
+            //todo test that
+            throw new ServiceException(previous: $serviceException);
+        } catch (Exception $e) {
+            throw DateException::wrongIntervalString();
+        }
+        $serviceStartTime = DateTime::createFromFormat("Y-m-d G:i", $dateStr . " " . $workingTimes["startTime"]);
+        $serviceEndTime = DateTime::createFromFormat("Y-m-d G:i", $dateStr . " " . $workingTimes["endTime"]);
+        $currentTime = new DateTime();
+        $availableSlots = [];
+        do {
+            $generatedSlot = new Interval($serviceStartTime->format('H:i'), $serviceDuration, $waitInterval);
+            // check if the service can be added due it's time, because if it's already too late we can't add it
+            $slotStartTime = clone $serviceStartTime; // creates a new object
+            if ($currentTime >= $slotStartTime->sub($bookableUntilInterval)) {
+                $serviceStartTime->add($totalInterval);
+                continue;
+            }
+            $isFree = true;
+            foreach ($bookedSlots as $appointment) {
+                if ($generatedSlot->getStartTime() < $appointment["startTime"] && $generatedSlot->getEndTime() <= $appointment["startTime"] ||
+                    $generatedSlot->getStartTime() > $appointment["startTime"] && $generatedSlot->getStartTime() >= $appointment["endTime"]) {
+                    // lo slot potrebbe essere libero
+                    //todo rimuovere la terza condizione ($generatedSlot->getStartTime() > $o["startTime"])
+                } else {
+                    // lo slot non è libero
+                    //TODO Add break?
+                    $isFree = false;
+                }
+            }
+            // TODO raggruppare questo if
+            // se isFree è rimasto true significa che lo slot è compatibile con tutti gli appuntamenti gia presi
+            // quindi può essere inserito
+            if ($isFree) {
+                // we need to check if the end time of a slot is bigger than the actual service end time
+                if ($generatedSlot->getEndTime() <= $serviceEndTime->format('H:i')) {
+                    // we can add the slot
+                    // todo check if we are removing the wait time
+                    // before adding to the generated slot we remove the wait time because it shouldn't be visible by the client
+                    $availableSlots[] = $generatedSlot->getArray();
+                }
+            }
+            $serviceStartTime->add($totalInterval);
+        } while ($serviceStartTime < $serviceEndTime);
+        // if there is a holidays remove some slots
+        // get holiday start and end time
+        $holidayTimes = workingTimes::getHolidayTimes($db, $weekDay, $dateStr, $serviceId, $employeeId);
+        return self::removeHolidayTime($availableSlots, $holidayTimes);
+    }
+
+    private static function removeHolidayTime($slots, $holidays){
+        $slotsWithHolidays = [];
+        foreach ($holidays as $holiday) {
+            foreach ($slots as $slot) {
+                // add only the slots that arent included in the holiday timeframe
+                if ($slot["startTime"] <= $holiday['startTime'] && $slot['endTime'] <= $holiday['startTime']) {
+                    $slotsWithHolidays[] = $slot;
+                } elseif ($slot["startTime"] >= $holiday['endTime']) {
+                    $slotsWithHolidays[] = $slot;
+                }
+            }
+            $slots = $slotsWithHolidays;
+            $slotsWithHolidays = [];
+        }
+        return $slots;
     }
 }
