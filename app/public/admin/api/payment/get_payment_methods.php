9c06fa26513d684612ea571d5d6889aa8f20f540
Daniele Pintore <daniele.pintore1@gmail.com>
2022-08-13 16:54:19 +0200

    Beautify payment section of admin api

--- app/public/admin/api/payment/get_payment_methods.php
+++ app/public/admin/api/payment/get_payment_methods.php
@@ -5,15 +5,14 @@ use Admin\User;
 
 require_once(realpath(dirname(__FILE__, 5)) . '/src/Api/loader.php');
 session_start();
-if (session_status() == PHP_SESSION_ACTIVE &&  isset($_SESSION['logged']) && $_SESSION['logged'] && $_SESSION['isAdmin']) {
+if (session_status() == PHP_SESSION_ACTIVE && isset($_SESSION['logged']) && $_SESSION['logged'] && $_SESSION['isAdmin']) {
     // user is logged
     // create user object
     $db = new Database();
-
     $user = new User($db);
     // check if user still exist in the database and is in active status
-    if (!$user->exist() || !$user->isActive()){
-        if (DEBUG){
+    if (!$user->exist() || !$user->isActive()) {
+        if (DEBUG) {
             print("The user no longer exist");
         } else {
             print(json_encode(array("error" => true)));
@@ -21,13 +20,14 @@ if (session_status() == PHP_SESSION_ACTIVE &&  isset($_SESSION['logged']) && $_S
         die(0);
     }
     try {
-            $paymentMethods = Payment::getPaymentMethods($db);
+        $paymentMethods = Payment::getPaymentMethods($db);
         // se non ci sono stati errori fornisci la risposta
         if (count($paymentMethods) == 0) {
             print(json_encode(array()));
         } else {
             print(json_encode($paymentMethods));
         }
+        die(0);
     } catch (DatabaseException|Exception $e) {
         if (DEBUG) {
             Debug::printException($e);
@@ -39,7 +39,7 @@ if (session_status() == PHP_SESSION_ACTIVE &&  isset($_SESSION['logged']) && $_S
 } else {
     // user isn't logged
     // display error
-    if (DEBUG){
+    if (DEBUG) {
         print("There is no current logged user");
     } else {
         print(json_encode(array("error" => true)));
