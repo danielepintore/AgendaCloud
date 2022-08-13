047f5619c1db8a0b796f49752d9acfb6df6cb5e2
Daniele Pintore <daniele.pintore1@gmail.com>
2022-08-13 17:02:51 +0200

    Beautify employee section of admin api

--- app/public/admin/api/employee/delete_employee.php
+++ app/public/admin/api/employee/delete_employee.php
@@ -4,15 +4,14 @@ use Admin\User;
 
 require_once(realpath(dirname(__FILE__, 5)) . '/src/Api/loader.php');
 session_start();
-if (session_status() == PHP_SESSION_ACTIVE &&  isset($_SESSION['logged']) && $_SESSION['logged'] && $_SESSION['isAdmin']) {
+if (session_status() == PHP_SESSION_ACTIVE && isset($_SESSION['logged']) && $_SESSION['logged'] && $_SESSION['isAdmin']) {
     // user is logged
     // create user object
     $db = new Database();
-    
     $user = new User($db);
     // check if user still exist in the database and is in active status
-    if (!$user->exist() || !$user->isActive()){
-        if (DEBUG){
+    if (!$user->exist() || !$user->isActive()) {
+        if (DEBUG) {
             print("The user no longer exist");
         } else {
             print(json_encode(array("error" => true)));
@@ -29,6 +28,7 @@ if (session_status() == PHP_SESSION_ACTIVE &&  isset($_SESSION['logged']) && $_S
             } else {
                 print(json_encode(array("error" => true)));
             }
+            die(0);
         } catch (DatabaseException|Exception $e) {
             if (DEBUG) {
                 Debug::printException($e);
@@ -40,16 +40,15 @@ if (session_status() == PHP_SESSION_ACTIVE &&  isset($_SESSION['logged']) && $_S
     } else {
         if (DEBUG) {
             print("Something isn't setted up");
-            die(0);
         } else {
             print(json_encode(array("error" => true)));
-            die(0);
         }
+        die(0);
     }
 } else {
     // user isn't logged
     // display error
-    if (DEBUG){
+    if (DEBUG) {
         print("There is no current logged user");
     } else {
         print(json_encode(array("error" => true)));
--- app/public/admin/api/employee/get_employees.php
+++ app/public/admin/api/employee/get_employees.php
@@ -5,15 +5,14 @@ use Admin\Employee;
 
 require_once(realpath(dirname(__FILE__, 5)) . '/src/Api/loader.php');
 session_start();
-if (session_status() == PHP_SESSION_ACTIVE &&  isset($_SESSION['logged']) && $_SESSION['logged'] && $_SESSION['isAdmin']) {
+if (session_status() == PHP_SESSION_ACTIVE && isset($_SESSION['logged']) && $_SESSION['logged'] && $_SESSION['isAdmin']) {
     // user is logged
     // create user object
     $db = new Database();
-    
     $user = new User($db);
     // check if user still exist in the database and is in active status
-    if (!$user->exist() || !$user->isActive()){
-        if (DEBUG){
+    if (!$user->exist() || !$user->isActive()) {
+        if (DEBUG) {
             print("The user no longer exist");
         } else {
             print(json_encode(array("error" => true)));
@@ -32,6 +31,7 @@ if (session_status() == PHP_SESSION_ACTIVE &&  isset($_SESSION['logged']) && $_S
         } else {
             print(json_encode($employees));
         }
+        die(0);
     } catch (DatabaseException|Exception $e) {
         if (DEBUG) {
             Debug::printException($e);
@@ -43,7 +43,7 @@ if (session_status() == PHP_SESSION_ACTIVE &&  isset($_SESSION['logged']) && $_S
 } else {
     // user isn't logged
     // display error
-    if (DEBUG){
+    if (DEBUG) {
         print("There is no current logged user");
     } else {
         print(json_encode(array("error" => true)));
