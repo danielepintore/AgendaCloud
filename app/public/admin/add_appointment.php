57b744927398566dc7609cbbaf66a9455031294e
Daniele Pintore <daniele.pintore1@gmail.com>
2022-08-15 12:27:06 +0200

    Remove a duplicate tag

--- app/public/admin/add_appointment.php
+++ app/public/admin/add_appointment.php
@@ -25,7 +25,7 @@ if (session_status() == PHP_SESSION_ACTIVE && isset($_SESSION['logged']) && $_SE
 }
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="it">
 <head>
     <title><?php print("Aggiungi un appuntamento - " . $config->company->name . " - AgendaCloud"); ?></title>
     <link rel="apple-touch-icon" sizes="180x180" href="../img/favicon/apple-touch-icon.png">
@@ -49,121 +49,119 @@ if (session_status() == PHP_SESSION_ACTIVE && isset($_SESSION['logged']) && $_SE
 <body>
 <div class="container">
     <?php Navbar::printNavBar($user, APPOINTMENT); ?>
-    <div class="container">
-        <div class="row">
-            <!--Servizi-->
-            <div class="col-12 col-md-12 mt-4">
-                <!--Card servizi-->
-                <div class="card">
-                    <div class="card-header">
-                        Servizi disponibili:
-                    </div>
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
-                                <h5 class="card-title">Scegli un servizio:</h5>
-                                <select id="tipoServizio" class="form-select mb-2" aria-label="Default select example">
-                                    <option value="-1" selected disabled hidden>Seleziona un servizio</option>
-                                    <?php
-                                    try {
-                                        $services = \Admin\Services::getEmployeeService($db, $user->IsAdmin(), $user->getId());
-                                        // se non è presente un errore
-                                        foreach ($services as $s) {
-                                            print('<option value="' . $s["id"] . '">' . $s["Nome"] . '</option>');
-                                        }
-                                    } catch (DatabaseException|Exception $e) {
-                                        header("HTTP/1.1 303 See Other");
-                                        header("Location: /error.php");
+    <div class="row">
+        <!--Servizi-->
+        <div class="col-12 col-md-12 mt-2">
+            <!--Card servizi-->
+            <div class="card">
+                <div class="card-header">
+                    Servizi disponibili:
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                            <h5 class="card-title">Scegli un servizio:</h5>
+                            <select id="tipoServizio" class="form-select mb-2" aria-label="Default select example">
+                                <option value="-1" selected disabled hidden>Seleziona un servizio</option>
+                                <?php
+                                try {
+                                    $services = \Admin\Services::getEmployeeService($db, $user->IsAdmin(), $user->getId());
+                                    // se non è presente un errore
+                                    foreach ($services as $s) {
+                                        print('<option value="' . $s["id"] . '">' . $s["Nome"] . '</option>');
                                     }
-                                    ?>
-                                </select>
-                            </div>
-                        </div>
-                        <?php if ($user->IsAdmin()) { ?>
-                            <div id="scelta_dipendente">
-                                <h5 class="card-title mt-2">Scegli un dipendente:</h5>
-                                <select id="lista_dipendenti" class="form-select" disabled="true">
-                                    <option value="-1" selected disabled hidden>Devi selezionare un servizio</option>
-                                </select>
-                            </div>
-                        <?php } ?>
-                        <div id="info-servizio" class="mt-2">
-                            <span>Durata servizio: </span>
-                            <span id="time-lenght"></span>
-                            <span>costo servizio: </span>
-                            <span id="prezzo-servizio"></span>
+                                } catch (DatabaseException|Exception $e) {
+                                    header("HTTP/1.1 303 See Other");
+                                    header("Location: /error.php");
+                                }
+                                ?>
+                            </select>
                         </div>
                     </div>
+                    <?php if ($user->IsAdmin()) { ?>
+                        <div id="scelta_dipendente">
+                            <h5 class="card-title mt-2">Scegli un dipendente:</h5>
+                            <select id="lista_dipendenti" class="form-select" disabled="true">
+                                <option value="-1" selected disabled hidden>Devi selezionare un servizio</option>
+                            </select>
+                        </div>
+                    <?php } ?>
+                    <div id="info-servizio" class="mt-2 d-none">
+                        <span>Durata servizio: </span>
+                        <span id="time-lenght"></span>
+                        <span>costo servizio: </span>
+                        <span id="prezzo-servizio"></span>
+                    </div>
                 </div>
             </div>
-            <!--Calendar-->
-            <div class="col-auto calendar-col mt-4">
-                <div id="bookings-calendar" class="calendar">
-                    <div class="calendar-header">
-                        <i class="icon-chevron fa-solid fa-chevron-left"></i>
-                        <h1></h1>
-                        <i class="icon-chevron fa-solid fa-chevron-right"></i>
-                    </div>
-                    <div class="calendar-weekdays"></div>
-                    <div class="calendar-content"></div>
+        </div>
+        <!--Calendar-->
+        <div class="col-auto calendar-col mt-4">
+            <div id="bookings-calendar" class="calendar">
+                <div class="calendar-header">
+                    <i class="icon-chevron fa-solid fa-chevron-left"></i>
+                    <h1></h1>
+                    <i class="icon-chevron fa-solid fa-chevron-right"></i>
+                </div>
+                <div class="calendar-weekdays"></div>
+                <div class="calendar-content"></div>
+            </div>
+        </div>
+        <!--Orari-->
+        <div class="col-12 col-md mt-4">
+            <!--Card servizi-->
+            <div class="card" id="orari">
+                <div class="card-header">
+                    Orari disponibili:
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Scegli un orario da qua sotto:</h5>
+                    <select id="lista-orari" class="form-select">
+                    </select>
                 </div>
             </div>
-            <!--Orari-->
-            <div class="col-12 col-md mt-4">
-                <!--Card servizi-->
-                <div class="card" id="orari">
-                    <div class="card-header">
-                        Orari disponibili:
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Scegli un orario da qua sotto:</h5>
-                        <select id="lista-orari" class="form-select">
-                        </select>
-                    </div>
+            <!--Card i tuoi dati-->
+            <div class="card mt-4" id="dati_personali">
+                <div class="card-header">
+                    I dati del cliente:
                 </div>
-                <!--Card i tuoi dati-->
-                <div class="card mt-4" id="dati_personali">
-                    <div class="card-header">
-                        I dati del cliente:
-                    </div>
-                    <div class="card-body">
-                        <form id="form_dati_personali">
-                            <div class="row">
-                                <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6 mb-2">
-                                    <input type="text" class="form-control" id="nomeInput" name="nomeInput"
-                                           placeholder="Nome">
-                                </div>
-                                <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6">
-                                    <input type="text" class="form-control" id="cognomeInput" name="cognomeInput"
-                                           placeholder="Cognome">
-                                </div>
+                <div class="card-body">
+                    <form id="form_dati_personali">
+                        <div class="row">
+                            <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6 mb-2">
+                                <input type="text" class="form-control" id="nomeInput" name="nomeInput"
+                                       placeholder="Nome">
                             </div>
-                            <div class="row mt-2">
-                                <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6 mb-2">
-                                    <input type="email" class="form-control" id="emailInput" name="emailInput"
-                                           placeholder="Email">
-                                </div>
-                                <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6">
-                                    <input type="tel" class="form-control" id="phoneInput" name="phoneInput"
-                                           placeholder="Cellulare">
-                                </div>
+                            <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6">
+                                <input type="text" class="form-control" id="cognomeInput" name="cognomeInput"
+                                       placeholder="Cognome">
                             </div>
-                        </form>
-                    </div>
+                        </div>
+                        <div class="row mt-2">
+                            <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6 mb-2">
+                                <input type="email" class="form-control" id="emailInput" name="emailInput"
+                                       placeholder="Email">
+                            </div>
+                            <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6">
+                                <input type="tel" class="form-control" id="phoneInput" name="phoneInput"
+                                       placeholder="Cellulare">
+                            </div>
+                        </div>
+                    </form>
                 </div>
-                <form id="paymentForm">
-                    <input type="hidden" id="dayPOST" name="date">
-                    <input type="hidden" id="idServicePOST" name="serviceId">
-                    <input type="hidden" id="employeeIdPOST" name="employeeId">
-                    <input type="hidden" id="slotPOST" name="slot">
-                    <input type="hidden" id="clientNomePOST" name="clientNome">
-                    <input type="hidden" id="clientCognomePOST" name="clientCognome">
-                    <input type="hidden" id="clientEmailPOST" name="clientEmail">
-                    <input type="hidden" id="clientPhonePOST" name="clientPhone">
-                    <input type="button" id="prenota_btn" class="btn btn-success mt-4 mb-4 w-100"
-                           value="Prenota" disabled>
-                </form>
             </div>
+            <form id="paymentForm">
+                <input type="hidden" id="dayPOST" name="date">
+                <input type="hidden" id="idServicePOST" name="serviceId">
+                <input type="hidden" id="employeeIdPOST" name="employeeId">
+                <input type="hidden" id="slotPOST" name="slot">
+                <input type="hidden" id="clientNomePOST" name="clientNome">
+                <input type="hidden" id="clientCognomePOST" name="clientCognome">
+                <input type="hidden" id="clientEmailPOST" name="clientEmail">
+                <input type="hidden" id="clientPhonePOST" name="clientPhone">
+                <input type="button" id="prenota_btn" class="btn btn-success mt-4 mb-4 w-100"
+                       value="Prenota" disabled>
+            </form>
         </div>
     </div>
 </div>
